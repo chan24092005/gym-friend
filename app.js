fe344107cac095a609ed418444d38feb51711bf0
chan24092005 <ben.chan@brandcrafter.com.hk>
2017-07-03 00:22:42 +0800

    save

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const express = require('express')
 const app = express()
 
+var bodyParser  = require('body-parser');
+var morgan      = require('morgan');
+var mongoose    = require('mongoose');
+
+var jwt    = require('jsonwebtoken'); // used to create, sign, and verify tokens
+var config = require('./config'); // get our config file
+var User   = require('./app/models/user'); // get our mongoose model
+
+
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
@@ -8,3 +17,113 @@ app.get('/', function (req, res) {
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
+
+
+// =======================
+// configuration =========
+// =======================
+var port = process.env.PORT || 8080; // used to create, sign, and verify tokens
+mongoose.connect(config.database); // connect to database
+app.set('superSecret', config.secret); // secret variable
+
+// use body parser so we can get info from POST and/or URL parameters
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// use morgan to log requests to the console
+app.use(morgan('dev'));
+
+// =======================
+// routes ================
+// =======================
+// basic route
+app.get('/', function(req, res) {
+    res.send('Hello! The API is at http://localhost:' + port + '/api');
+});
+
+// API ROUTES -------------------
+// we'll get to these in a second
+
+// =======================
+// start the server ======
+// =======================
+app.listen(port);
+console.log('Magic happens at http://localhost:' + port);
+
+
+// var insertDocuments = function(db, callback) {
+//   // Get the documents collection
+//   var collection = db.collection('documents');
+//   // Insert some documents
+//   collection.insertMany([
+//     {a : 1}, {a : 2}, {a : 3}
+//   ], function(err, result) {
+//     assert.equal(err, null);
+//     assert.equal(3, result.result.n);
+//     assert.equal(3, result.ops.length);
+//     console.log("Inserted 3 documents into the document collection");
+//     callback(result);
+//   });
+// }
+//
+// var updateDocument = function(db, callback) {
+//   // Get the documents collection
+//   var collection = db.collection('documents');
+//   // Update document where a is 2, set b equal to 1
+//   collection.updateOne({ a : 2 }
+//     , { $set: { b : 1 } }, function(err, result) {
+//     assert.equal(err, null);
+//     assert.equal(1, result.result.n);
+//     console.log("Updated the document with the field a equal to 2");
+//     callback(result);
+//   });
+// }
+//
+// var deleteDocument = function(db, callback) {
+//   // Get the documents collection
+//   var collection = db.collection('documents');
+//   // Insert some documents
+//   collection.deleteOne({ a : 3 }, function(err, result) {
+//     assert.equal(err, null);
+//     assert.equal(1, result.result.n);
+//     console.log("Removed the document with the field a equal to 3");
+//     callback(result);
+//   });
+// }
+//
+// var findDocuments = function(db, callback) {
+//   // Get the documents collection
+//   var collection = db.collection('documents');
+//   // Find some documents
+//   collection.find({}).toArray(function(err, docs) {
+//     assert.equal(err, null);
+//     // assert.equal(2, docs.length);
+//     console.log("Found the following records");
+//     console.dir(docs);
+//     callback(docs);
+//   });
+// }
+//
+// var MongoClient = require('mongodb').MongoClient
+//   , assert = require('assert');
+//
+// // Connection URL
+// var url = 'mongodb://localhost:27017/myproject';
+// // Use connect method to connect to the Server
+// MongoClient.connect(url, function(err, db) {
+//   assert.equal(null, err);
+//   console.log("Connected correctly to server");
+//
+//   insertDocuments(db, function() {
+//     updateDocument(db, function() {
+//       deleteDocument(db, function() {
+//         findDocuments(db, function() {
+//           db.close();
+//         });
+//       });
+//     });
+//   });
+// });
